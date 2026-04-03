4355782ea6c1eebc651650b2ae4eee1b10bd0ced
lastjung <chul.h.jung@gmail.com>
2026-04-03 09:32:10 -0400

    refactor: remove 3d-sorting project files

--- threejs-projects/7_3d-sorting/app.js
+++
@@ -1,7 +0,0 @@
-import { initVisualizer } from './visualizer.js';
-import { initController } from './controller.js';
-
-const canvas = document.getElementById('three-canvas');
-if (initVisualizer(canvas)) {
-  initController();
-}
